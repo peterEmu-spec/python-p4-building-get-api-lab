3eaec1c220d2b206012675020418f73b06013678
Peter Emu <emu.kimani@strathmore.edu>
2026-01-13 15:58:03 +0300

    Completed Bakery GET API routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -11,28 +9,40 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
 
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(bakeries, 200)
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    return make_response(bakery.to_dict(), 200)
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [
+        bg.to_dict()
+        for bg in BakedGood.query.order_by(BakedGood.price.desc()).all()
+    ]
+    return make_response(baked_goods, 200)
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    baked_good = (
+        BakedGood.query
+        .order_by(BakedGood.price.desc())
+        .first()
+    )
+    return make_response(baked_good.to_dict(), 200)
